f9e650a13b55fa63b6b4d0c27eaec4eab013a373
jacintojosh <jacintojosh@gmail.com>
2020-12-20 15:05:28 +0800

    Also check if variable has a number value, added previous solution, grammar edits.

--- eval-prefix-expression/evalPrefixExpression.js
+++ eval-prefix-expression/evalPrefixExpression.js
@@ -1,4 +1,5 @@
 // This solution uses tree concepts to simulate nesting in prefix operations.
+// This solution assumes that the expression does not have variables that have variables as value i.e. '+ 2 a' where in the var object a = b and where b = 2.
 const evalPreFix = (expression, variables) => {
   function nodeObject(value) {
     this.value = value
@@ -50,8 +51,8 @@ const evalPreFix = (expression, variables) => {
       } else {
         curr.number = e
       }
-    // If you don't get a number, check if its a variable then get the variables number
-    } else if (variables && variables.hasOwnProperty(e)) {
+    // If you don't get a number, check if its a variable, then check if the variable value is a number
+    } else if (variables && variables.hasOwnProperty(e) && Number.isInteger(Number(variables[e]))) {
       if (curr.number) {
         evalNum = Math.floor(eval(curr.number + curr.value + variables[e]))
         if (curr.prev) {
@@ -72,7 +73,7 @@ const evalPreFix = (expression, variables) => {
       } else {
         curr.number = variables[e]
       }
-    // We got something unexpected so we just terminate the program
+    // We got something unexpected so we terminate the program
     } else {
       return null
     }
@@ -84,9 +85,93 @@ const evalPreFix = (expression, variables) => {
   }
 }
 
+// Tests
 console.log(evalPreFix('+ 6 * - 4 + 2 3 8'))
 console.log(evalPreFix('- + * 2 3 * 5 4 9'))
 console.log(evalPreFix('* * * 2 2 1'))
 console.log(evalPreFix('+ 1 2 * 5'))
 console.log(evalPreFix('+ 1 x', {x:2}))
 console.log(evalPreFix('+ 1 x'))
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Previous solution -- Uses stacks! Logic is wrong because this solution can't fully simulate nested operations.
+// Including this for documentation, might try and fix it in the future.
+const evalPreFixPrev = (expression, variables) => {
+  const expArr = expression.split(' ')
+  const operators = ['+', '-', '/', '*']
+  const operatorStack = []
+  const numberStack = []
+  let wasNumber = false
+  let last = null
+
+  expArr.forEach(e => {
+    // If we see an operator, add it to the stack
+    if (operators.includes(e)) {
+      operatorStack.push(e)
+      wasNumber = false
+    // If number, add the number to the stack if the previous element was an operator
+    // else process current stack if the previous element was a number
+    // after processing the number, process previous numbers if there are more than 2 numbers in the stack
+    } else if (isNumber(e)) {
+      if (numberStack.length > 0 && operatorStack.length > 0 && wasNumber) {
+        numberStack.push(eval(numberStack.pop() + operatorStack.pop() + e))
+        // Only checking if  there are more than 2 numbers to avoid processing earlier numbers
+        while(numberStack.length > 2 && operatorStack.length > 0) {
+          last = numberStack.pop()
+          numberStack.push(eval(numberStack.pop() + operatorStack.pop() + last))
+        }
+        wasNumber = true
+      } else {
+        numberStack.push(e)
+        wasNumber = true
+      }
+    // If not number, check if it is a variable
+    } else if (variables && variables.hasOwnProperty(e)) {
+      if (numberStack.length > 0 && operatorStack.length > 0 && wasNumber) {
+        numberStack.push(eval(numberStack.pop() + operatorStack.pop() + variables[e]))
+        // Only checking if  there are more than 2 numbers to avoid processing earlier numbers
+        while(numberStack.length > 2 && operatorStack.length > 0) {
+          last = numberStack.pop()
+          numberStack.push(eval(numberStack.pop() + operatorStack.pop() + last))
+        }
+        wasNumber = true
+      } else {
+        numberStack.push(variables[e])
+        wasNumber = true
+      }
+    // Return null if we get anything invalid
+    } else {
+      return null
+    }
+  })
+
+  // Process the last few numbers.
+  while(numberStack.length > 1 && operatorStack.length > 0) {
+    last = numberStack.pop()
+    numberStack.push(eval(numberStack.pop() + operatorStack.pop() + last))
+  }
+  
+  // Check if everything has been processed, if not return null
+  // Also return null if there are excess operators
+  if (operatorStack.length > 0 || numberStack.length > 1) {
+    return null
+  } else {
+    return numberStack[0]
+  }
+  
+}
+
+const isNumber = (number) => {
+  return Number.isInteger(Number(number))
+}
+
+
+// Fails in 2nd test.
+console.log('prev')
+console.log(evalPreFixPrev('+ 6 * - 4 + 2 3 8'))
+console.log(evalPreFixPrev('- + * 2 3 * 5 4 9'))
+console.log(evalPreFixPrev('* * * 2 2 1'))
+console.log(evalPreFixPrev('+ 1 2 * 5'))
+console.log(evalPreFixPrev('+ 1 x', {x:2}))
+console.log(evalPreFixPrev('+ 1 x'))
